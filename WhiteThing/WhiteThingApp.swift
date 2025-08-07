63f81fc1325c28019592fb2918422dea096fa248
Samuel Sullins <samuelasullins@gmail.com>
2025-08-07 12:36:12 -0600

    Initial Commit

---
+++ WhiteThing/WhiteThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteThingApp.swift
+//  WhiteThing
+//
+//  Created by Samuel Sullins on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WhiteThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
